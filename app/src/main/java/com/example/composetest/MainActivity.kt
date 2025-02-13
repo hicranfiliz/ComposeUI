56165320a2ce9b630e6b3f87370272e8497bdb88
hicranfiliz <h.filiz781@gmail.com>
2025-02-13 20:24:01 +0300

    superscript subscript

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -8,11 +8,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,33 +29,46 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CustomText()
+                    
                 }
             }
         }
     }
 
-
-    @Composable
-    fun CustomText() {
-            SelectionContainer {
-                Column {
-                    Text("Hello Compose!!!")
-                    Text("Hello Compose!!!")
-                    DisableSelection {
-                        Text("Hello Compose!!!")
-                    }
-                    Text("Hello Compose!!!")
-                }
+    //buildannotatedString: metnin bir bolumunu farkli stillendirmek icin
+@Composable
+fun SuperScriptText(
+    normalText : String,
+    normalFontSize: TextUnit = MaterialTheme.typography.bodyLarge.fontSize,
+    superText: String,
+    superTextFontSize: TextUnit  = MaterialTheme.typography.bodySmall.fontSize,
+    superTextFontWeight: FontWeight = FontWeight.Normal
+) {
+    Text(buildAnnotatedString {
+        withStyle(
+            style = SpanStyle(
+                fontSize = normalFontSize
+            )
+        ) {
+            append(normalText)
         }
-
-    }
+        withStyle(
+            style = SpanStyle(
+                fontSize = superTextFontSize,
+                fontWeight = superTextFontWeight,
+                baselineShift = BaselineShift.Superscript
+            )
+        ){
+            append(superText)
+        }
+    })
+}
 
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
         ComposeTestTheme {
-            CustomText()
+            SuperScriptText(normalText = "Hello", superText = "Compose", superTextFontWeight = FontWeight.Bold)
         }
     }
 }
