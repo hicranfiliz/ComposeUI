db5c9912f2455b1b476c6cce61b01c6b2f887dd0
hicranfiliz <h.filiz781@gmail.com>
2025-02-13 20:08:09 +0300

    text selection

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -4,39 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.text.selection.DisableSelection
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,68 +22,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Text(
-                            stringResource(R.string.app_name), modifier = Modifier
-                                .background(MaterialTheme.colorScheme.primary)
-                                .padding(16.dp)
-                        )
-                    }
+                    CustomText()
                 }
             }
         }
     }
 
+
     @Composable
     fun CustomText() {
-        Text(
-            stringResource(R.string.app_name), modifier = Modifier
-                .background(MaterialTheme.colorScheme.primary)
-                .padding(16.dp)
-                .width(200.dp),
-            color = Color.White,
-            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.SemiBold,
-            textAlign = TextAlign.Center
-        )
-    }
-
-    // Textteki her bir charin nasil gorunecegini belirler buildAnnotatedString..
-    @Composable
-    fun CustomText2() {
-        val primaryColor = MaterialTheme.colorScheme.primary
-        Text(
-            buildAnnotatedString {
-                withStyle(style = ParagraphStyle(textAlign = TextAlign.Center)) {
-                    withStyle(
-                        style = SpanStyle(color = primaryColor,
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold)) {
-                        append("A")
+            SelectionContainer {
+                Column {
+                    Text("Hello Compose!!!")
+                    Text("Hello Compose!!!")
+                    DisableSelection {
+                        Text("Hello Compose!!!")
                     }
-                    append("B")
-                    append("C")
-                    append("D")
-                    append("E")
+                    Text("Hello Compose!!!")
                 }
-            }, modifier = Modifier.width(200.dp)
-        )
-    }
+        }
 
-    @Composable
-    fun CustomText3() {
-        Text("Hello World".repeat(20), maxLines = 2, overflow = TextOverflow.Ellipsis)
     }
-
 
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
         ComposeTestTheme {
-            Column(modifier = Modifier.fillMaxSize()) {
-                CustomText3()
-            }
+            CustomText()
         }
     }
 }
