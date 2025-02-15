086be06608d371d9f380352537af68ff96d2cff7
hicranfiliz <h.filiz781@gmail.com>
2025-02-15 14:22:49 +0300

    Coil- Image loading library

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.composetest
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.Text
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,29 +33,56 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   GoogleButton(
-                       text = "Sign Up with Google",
-                       loadingText = "Creating Account",
-                       onClicked = {
-                           Log.d("google button clicked", "mytagg")
-                       }
-                   )
+                   Column(
+                       modifier = Modifier.fillMaxSize(),
+                       verticalArrangement = Arrangement.Center,
+                       horizontalAlignment = Alignment.CenterHorizontally
+                   ) {
+                       CoilImage()
+                   }
                 }
             }
         }
     }
 
-
+    @Composable
+    fun CoilImage(){
+        Box(
+            modifier = Modifier
+                .height(150.dp)
+                .width(150.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            val context = LocalContext.current
+            val painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(context)
+                    .data("https://avatars.githubusercontent.com/u/14994036?v=4")
+                    .placeholder(R.drawable.paper_plane)
+                    .error(R.drawable.minus)
+                    .crossfade(1000)
+                    .transformations(CircleCropTransformation())
+                    .diskCachePolicy(CachePolicy.ENABLED)
+                    .build()
+            )
+            var painterState = painter.state
+            Image(painter = painter, contentDescription = "Logo Image")
+//            if (painterState is AsyncImagePainter.State.Loading){
+//                CircularProgressIndicator()
+//            }
+        }
+    }
 
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
         ComposeTestTheme {
-            GoogleButton(
-                text = "Sign Up with Google",
-                loadingText = "Creating Account",
-                onClicked = {}
-            )
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CoilImage()
+            }
         }
     }
 }
